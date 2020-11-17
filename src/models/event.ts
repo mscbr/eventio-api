182d3705f1ded26c2a0961364de9bb960ec2c58e
mscbr <scibor.maciej@gmail.com>
2020-11-17 22:05:29 +0100

    Event, User models

--- src/models/event.ts
+++ src/models/event.ts
@@ -1,25 +1,6 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model, Document } from 'mongoose';
 
 import { IUser } from './user';
-
-export const EventModel = model(
-  'Event',
-  new Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    startsAt: { type: Date, min: Date.now, required: true },
-    capacity: { type: Number, min: 1, required: true },
-    owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    attendees: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
-    createdAt: { type: Date, min: Date.now, default: Date.now },
-    updatedAt: {
-      type: Date,
-      min: Date.now,
-      default: Date.now,
-      setDefaultsOnInsert: true,
-    },
-  })
-);
 
 export interface IEvent {
   id: string;
@@ -27,7 +8,7 @@ export interface IEvent {
   description: string;
   startsAt: string; // ISO-formatted datetime
   capacity: number;
-  owner: IUser;
+  owner: string | IUser;
   attendees: IUser[] | [];
   createdAt: string; // ISO-formatted datetime
   updatedAt: string; // ISO-formatted datetime
@@ -44,3 +25,20 @@ export type TPatchEvent = {
   startsAt?: string; // ISO-formatted datetime
   capacity?: number;
 };
+
+type IEventDoc = IEvent & Document;
+
+export const Event = model<IEventDoc>(
+  'Event',
+  new Schema(
+    {
+      title: { type: String, required: true, unique: true },
+      description: { type: String, required: true },
+      startsAt: { type: Date, min: Date.now, required: true },
+      capacity: { type: Number, min: 1, required: true },
+      owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+      attendees: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
+    },
+    { timestamps: true }
+  )
+);
